39b303dc07df11dd4efc838d9c98fb8a75cb5981
Drew Chorney <drchorne@gmail.com>
2018-04-01 09:35:50 -0600

    added redirect

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -1,2 +1,7 @@
 class UiController < ApplicationController
+
+  #def index
+    #redirect_to "/client/index.html", :status => :moved_permanently
+    #redirect_to "https://drchorney.github.io/spa-demo/public/client/index.html"
+  #end
 end
